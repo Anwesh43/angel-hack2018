5b1af84f6a0d34890ebb7aea40de5492dd4f7df6
Anwesh43 <anweshthecool0@gmail.com>
2018-06-09 17:41:32 +0530

    adding server file

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+app.use((req,res,next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next()
+})
+
+const createRouterConfig = () => {
+    const postRoutes = require('./postRoutes')
+    const getRoutes  = require('./getRoutes')
+    const routerConfig = []
+    const pushToConfig = (routes, method) => {
+        routes.forEach((route) => {
+            console.log(routes)
+            routerConfig.push({method, cb : route.controller.handle, endpoint : route.endpoint})
+        })
+    }
+    pushToConfig(postRoutes, 'post')
+    pushToConfig(getRoutes, 'get')
+    return routerConfig
+}
+
+const Router = require('./router')
+
+const router = new Router(app, createRouterConfig())
+
+router.bind('/api')
+
+app.listen('9000', () => {
+    console.log("started listening on port 9000")
+})
